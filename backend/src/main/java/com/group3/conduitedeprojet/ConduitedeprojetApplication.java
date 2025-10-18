549853de3ad418924d9087356d79c9a49a5eb9da
Tourlat <foucaultfelix33@gmail.com>
2025-10-18 14:53:04 +0200

    Init backend (springboot) and frontend (vite + vue)

---
+++ backend/src/main/java/com/group3/conduitedeprojet/ConduitedeprojetApplication.java
@@ -0,0 +1,13 @@
+package com.group3.conduitedeprojet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConduitedeprojetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConduitedeprojetApplication.class, args);
+	}
+
+}
