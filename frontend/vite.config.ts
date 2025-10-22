0e2bea0a30dfe66280d4682e202e6e8e4cc95ed1
Kenzo Farrugia <kenzo.farrugia@etu.u-bordeaux.fr>
2025-10-22 17:06:55 +0200

    Add eslint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   const backendUrl = process.env.VITE_BACKEND_URL || 'http://localhost:8080'
 
   return {
