7a2e8e0b83fdae7b467805429321de891b1eb251
Justus Wendroth <justus.wendroth@outlook.de>
2025-10-23 00:15:40 +0200

    Fixed cors issues

--- backend/src/main/java/com/group3/conduitedeprojet/controller/HelloController.java
+++ backend/src/main/java/com/group3/conduitedeprojet/controller/HelloController.java
@@ -1,13 +1,9 @@
 package com.group3.conduitedeprojet.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
-
-  @Value("${app.frontend.url}")
-  private String frontendUrl;
 
   @GetMapping("/api/hello")
   public String hello() {
