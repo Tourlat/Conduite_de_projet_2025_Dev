bdd87c1913d4cddff7d1ef7add6191e57bdaadc1
Justus Wendroth <justus.wendroth@outlook.de>
2025-10-23 10:23:27 +0200

    Remove url from main.ts beacuse its already in .env

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8080";
 
 createApp(App).mount("#app");
