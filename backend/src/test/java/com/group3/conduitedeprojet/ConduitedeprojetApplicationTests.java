d55c7e4f7f0234d21fde5202bd06f1a6b77fae75
Justus Wendroth <justus.wendroth@outlook.de>
2025-10-27 15:26:26 +0100

    Remove unused test

--- backend/src/test/java/com/group3/conduitedeprojet/ConduitedeprojetApplicationTests.java
+++ backend/src/test/java/com/group3/conduitedeprojet/ConduitedeprojetApplicationTests.java
@@ -1,13 +1,8 @@
 package com.group3.conduitedeprojet;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ConduitedeprojetApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
