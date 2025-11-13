25c8b9efda76dfa6fb67ae7928e6e7b7b6f5a434
Tourlat <foucaultfelix33@gmail.com>
2025-11-13 20:39:44 +0100

    Feat: add files for sprint endpoints

---
+++ backend/src/main/java/com/group3/conduitedeprojet/exceptions/SprintNotFoundException.java
@@ -0,0 +1,7 @@
+package com.group3.conduitedeprojet.exceptions;
+
+public class SprintNotFoundException extends RuntimeException {
+  public SprintNotFoundException(String message) {
+    super(message);
+  }
+}
--- backend/src/main/java/com/group3/conduitedeprojet/models/Issue.java
+++ backend/src/main/java/com/group3/conduitedeprojet/models/Issue.java
@@ -68,6 +68,10 @@ public class Issue {
   @JoinColumn(name = "assignee_id", nullable = true, foreignKey = @ForeignKey(name = "fk_assignee"))
   private User assignee;
 
+  @ManyToOne
+  @JoinColumn(name = "sprint_id", nullable = true, foreignKey = @ForeignKey(name = "fk_issue_sprint"))
+  private Sprint sprint;
+
   public enum Priority {
     LOW,
     MEDIUM,
@@ -95,6 +99,10 @@ public class Issue {
 
     if (assignee != null) {
       builder.assigneeId(assignee.getId());
+    }
+
+    if (sprint != null) {
+      builder.sprintId(sprint.getId());
     }
 
     return builder.build();
